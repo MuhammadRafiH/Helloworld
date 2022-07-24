6920d706da3b8dbeea1560fc77e3cf247ae2f358
MuhammadRafiH <muhrafih12@gmail.com>
2022-07-24 18:28:12 +0700

    test

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Rafi web</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h2><?= $name; ?></h2>
+    <h3><?= $email; ?></h3>
+    <img src=<?= $image; ?> alt="<?= $email; ?>" width="200">
+<script src="script.js"></script>  
+</body>
+</html>
